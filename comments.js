9a02b3b012a6051a9ad89c1da1a9bff64e42f39f
Anuj Srivastava <52110836+anujsrivastava010783@users.noreply.github.com>
2024-11-16 10:02:06 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+// create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
